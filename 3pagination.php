584700b9d2da18a5e070d683c10b99815487ac54
paddelboot <>
2012-06-26 18:50:34 +0200

    Version 1.0

--- 3pagination.php
+++ 3pagination.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: 3pagination
  * Description: Reach any page with no more than 3 clicks
@@ -8,14 +7,16 @@
  * TextDomain: 3pagination
  * DomainPath: /l10n
  */
+
 // Load example CSS
 add_action( 'wp_enqueue_scripts', 'load_example_css' );
 
+// Example callback function
 function load_example_css () {
 	wp_enqueue_style( 'threepagination-css', plugins_url( 'examples/style.css', __FILE__ ) );
 }
 
-if ( !class_exists( 'threepagination' ) ) {
+if ( ! class_exists( 'threepagination' ) ) {
 
 	class threepagination {
 
@@ -33,22 +34,12 @@ if ( !class_exists( 'threepagination' ) ) {
 		 * 
 		 * @since 0.1a
 		 */
-		public static function get ( $pretty = TRUE, $num_items = FALSE, $per_page = FALSE, $labels = TRUE, $css = 'classic' ) {
+		public static function get ( $pretty = TRUE, $max_num_pages = FALSE, $labels = TRUE, $css = 'classic' ) {
 
 			global $wp_query, $wp;
-			
-			//p( $wp, "WP" );
 
-			//p( $wp_query, "WP-QUERY" );
-
-			// Get total items found
-			$num_items = ( FALSE == $num_items ) ? $wp_query->found_posts : $num_items;
-
-			// Get posts per page
-			$per_page = ( FALSE == $per_page ) ? $wp_query->get( 'posts_per_page' ) : $per_page;
-
-			// Calculate and round the total number of pages
-			$total_pages = $wp_query->max_num_pages;
+			// Get the page count
+			$total_pages = ( FALSE == $max_num_pages ) ? $wp_query->max_num_pages : $max_num_pages;
 
 			// No need for navi
 			if ( 1 == $total_pages )
@@ -102,20 +93,38 @@ if ( !class_exists( 'threepagination' ) ) {
 						}
 					}
 					break;
+					
+				default:
+					for ( $i = 1; $i <= 999; ++$i ) {
+
+						if ( $i == 1
+								OR ($i >= 100 AND substr( $i, -3, 2 ) == substr( $on_page, -3, 2 ))
+								OR ($i >= 100 AND is_int( $i / 10 ) AND substr( $i, -3, 1 ) == substr( $on_page, -3, 1 ))
+								OR (is_int( $i / 100 ))
+								OR ($i == $total_pages)
+						) {
+							$url = self::url( $wp, $i, $pretty );
+							$page_string .= ( $i == $on_page ) ? "<span class='page-numbers current'>" . $i . "</span>" : "<a href='" . $url . "'>" . $i . "</a>";
+							if ( $i < $total_pages ) {
+								$page_string .= "&nbsp;";
+							}
+						}
+					}
+					break;
 			}
 
 			// Navigation labels
 			if ( FALSE !== $labels ) {
 				if ( $on_page > 1 ) {
 					$i = $on_page - 1;
-					$url = self::url( $wp, $i, $pretty );
-					$page_string = " &nbsp;<a class='page-numbers' href='" . $url . "'>&laquo;</a>&nbsp;&nbsp;" . $page_string;
+					$page_string = "<a class='page-numbers label-first' href='" . self::url( $wp, 1, $pretty ) . "'>&laquo;</a>&nbsp;" . $page_string;
+					$page_string = "<a class='page-numbers label-previous' href='" . self::url( $wp, $i, $pretty ) . "'>&lsaquo;</a>&nbsp;" . $page_string;
 				}
 
 				if ( $on_page < $total_pages ) {
 					$i = $on_page + 1;
-					$url = self::url( $wp, $i, $pretty );
-					$page_string .= "&nbsp;&nbsp;<a class='page-numbers' href='" . $url . "'>&raquo;</a>&nbsp;";
+					$page_string .= "&nbsp;<a class='page-numbers label-next' href='" . self::url( $wp, $i, $pretty ) . "'>&rsaquo;</a>";
+					$page_string .= "&nbsp;<a class='page-numbers label-last' href='" . self::url( $wp, $total_pages, $pretty ) . "'>&raquo;</a>";
 				}
 			}
 
@@ -140,18 +149,18 @@ if ( !class_exists( 'threepagination' ) ) {
 		 * 
 		 * @since 0.1a
 		 */
-		public static function draw ( $pretty = TRUE, $num_items = FALSE, $per_page = FALSE, $labels = TRUE, $css = 'classic' ) {
+		public static function draw ( $pretty = TRUE, $max_num_pages = FALSE, $labels = TRUE, $css = 'classic' ) {
 
-			echo self::get( $pretty, $num_items, $per_page, $labels, $css );
+			echo self::get( $pretty, $max_num_pages, $labels, $css );
 		}
 		
 		/**
-		 * Create page link
+		 * Create link href
 		 * 
 		 * @param object $wp | WP object
 		 * @param int $i | current element
 		 * @param bool $pretty | pretty permalink structure. TRUE or FALSE, defaults to TRUE
-		 * @return string $url | the href attribute of our pagination element
+		 * @return string $url | the href attribute of our pagination element link
 		 */
 		private static function url( $wp, $i, $pretty ) {
 					
